c56ce29bfe2b90a4f401156b287791f131b67461
UReddy <user@username.com>
2021-05-17 16:59:11 +0530

    Basic concepts: rendering classes and lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,28 +4,33 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "http://picsum.photos/200",
-  };
-
-  styles = {
-    fontSize: "10px",
-    fontWeigh: "bold",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
-    //return <h1>Hello World</h1>//for single element.
-    //return <div><h1>Hello World</h1><button>Increment</button></div>;//keep in div if multiple elements
     return (
       <React.Fragment>
-        <h1 style={{ fontSize: 30 }}>Hello World</h1>
-        <img src={this.state.imageUrl} />
-        <span>{this.state.count}</span> {/*--Any javascript expressions */}
-        <span styles={this.styles} className="badge badge-primary m-2">
+        <span
+          styles={this.getBadgeClasses()}
+          className="badge badge-primary m-2"
+        >
           {this.formatCount()}
         </span>
-        {/*converts to class attribute in html*/}
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+        {/*  key attribute is required to uniquely identify it in virtual DOM by React */}
       </React.Fragment>
-    ); //keeping div is creating div inside the main div root, so using this
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
